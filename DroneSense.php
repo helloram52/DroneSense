b7a4b659173b95548113d0e3b8e87a2f2860b57c
helloram52 <helloram52@gmail.com>
2015-10-10 14:34:36 -0500

    Signed-off-by: helloram52 <helloram52@gmail.com>

--- DroneSense.php
+++ DroneSense.php
@@ -5,64 +5,26 @@
 <body>
 <?
 
+<?php
+// DB connection info
 
-function OpenConnection()
-{
-    try
-    {
-        $serverName = "tcp:djdux8m8ie.database.windows.net,1433";
-        $connectionOptions = array("Database"=>"DroneSense_db",
-            "UID"=>"DroneSense@djdux8m8ie", "PWD"=>"Micro\$oft");
-        $conn = sqlsrv_connect($serverName, $connectionOptions);
-        if($conn == false){
-            die(FormatErrors(sqlsrv_errors()));
-        }
-    }
-    catch(Exception $e)
-    {
-        echo("Error!");
-    }
+$host = "tcp:djdux8m8ie.database.windows.net,1433";
+$user = "DroneSense";
+$pwd = "Micro\$oft";
+$db = "DroneSense_db";
+try{
+    $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+}
+catch(Exception $e){
+    die(print_r($e));
 }
 
-function ReadData()
-{
-    try
-    {
-        $conn = OpenConnection();
-        echo $_SERVER['SERVER_ADDR'];
-        echo $conn;
-        $tsql = "SELECT * FROM DroneSense.Data";
-        //$tsql = "SELECT [Humid],[TempF],[Gas_Sensor],[__createdAt] FROM DroneSense.Data;";    
-        $getProducts = sqlsrv_query($conn, $tsql);
-        echo ("After the query execution");
-        echo $getProducts;
-        if ($getProducts == FALSE){
-            sqlsrv_free_stmt($getProducts);
-            sqlsrv_close($conn);    
-            echo ("Failed");
-            die(FormatErrors(sqlsrv_errors()));
-        }
-        $productCount = 1;
-
-        while($row = sqlsrv_fetch_array($getProducts, SQLSRV_FETCH_ASSOC) && $productCount<=20)
-        {
-            echo($row['Humid']);
-            echo($row['TempF']);
-            echo($row['Gas_Sensor']);
-            echo($row['__createdAt']);
-            echo("<br/>");
-            $productCount++;
-        }
-        sqlsrv_free_stmt($getProducts);
-        sqlsrv_close($conn);
-    }
-    catch(Exception $e)
-    {
-        echo("Error!");
-    }
-}
-
-ReadData();
+$sql_select = "SELECT * FROM Data";
+$stmt = $conn->query($sql_select);
+$result = $stmt->fetchAll()
+echo $result;
+print_r($result);
 
 ?>
     Hey, Its working!!
