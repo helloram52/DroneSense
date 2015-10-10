3ae28ac5be2b48e21f984fce2ea8491913ff71be
helloram52 <helloram52@gmail.com>
2015-10-10 09:03:29 -0500

    Signed-off-by: helloram52 <helloram52@gmail.com>

---
+++ DroneSense.php
@@ -0,0 +1,45 @@
+<?
+function OpenConnection()
+{
+    try
+    {
+        $serverName = "djdux8m8ie.database.windows.net,1433";
+        $connectionOptions = array("Database"=>"DroneSense_db",
+            "Uid"=>"DroneSense", "PWD"=>"Micro$oft");
+        $conn = sqlsrv_connect($serverName, $connectionOptions);
+        if($conn == false)
+            die(FormatErrors(sqlsrv_errors()));
+    }
+    catch(Exception $e)
+    {
+        echo("Error!");
+    }
+}
+
+function ReadData()
+{
+    try
+    {
+        $conn = OpenConnection();
+        $tsql = "SELECT Humid,TempC FROM DroneSense.";
+        $getProducts = sqlsrv_query($conn, $tsql);
+        if ($getProducts == FALSE)
+            die(FormatErrors(sqlsrv_errors()));
+        $productCount = 0;
+        while($row = sqlsrv_fetch_array($getProducts, SQLSRV_FETCH_ASSOC))
+        {
+            echo($row['Humid']);
+            echo($row['TempC']);
+            echo("<br/>");
+            $productCount++;
+        }
+        sqlsrv_free_stmt($getProducts);
+        sqlsrv_close($conn);
+    }
+    catch(Exception $e)
+    {
+        echo("Error!");
+    }
+}
+
+?>
