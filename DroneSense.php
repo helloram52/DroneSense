cbbc135a6d1f5cfc78fe808cf54cf1a1d7a36a7a
helloram52 <helloram52@gmail.com>
2015-10-10 14:38:18 -0500

    Signed-off-by: helloram52 <helloram52@gmail.com>

--- DroneSense.php
+++ DroneSense.php
@@ -8,10 +8,15 @@
 <?php
 // DB connection info
 
+echo $_SERVER['SERVER_ADDR'];
+echo "hi";
+error_reporting(-1);
+ini_set('display_errors', 'On');
+
 $host = "tcp:djdux8m8ie.database.windows.net,1433";
 $user = "DroneSense";
 $pwd = "Micro\$oft";
-$db = "DroneSense_db";
+$db = "DroneSense_db";o
 try{
     $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
     $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
@@ -20,7 +25,7 @@ catch(Exception $e){
     die(print_r($e));
 }
 
-$sql_select = "SELECT * FROM Data";
+$sql_select = "SELECT * FROM DroneSense.Data";
 $stmt = $conn->query($sql_select);
 $result = $stmt->fetchAll()
 echo $result;
