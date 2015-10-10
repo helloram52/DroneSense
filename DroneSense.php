ae900c5533dffc6e363c904ca81e39e72e0f5c2d
helloram52 <helloram52@gmail.com>
2015-10-10 16:16:03 -0500

    Final

--- DroneSense.php
+++ DroneSense.php
@@ -1,37 +1,136 @@
+<!DOCTYPE html>
 <html>
+
 <head>
-    <title>new page...</title>
+<title>Biological Drone Informatics</title>
+<link rel="stylesheet" href="styles.css">
 </head>
+
 <body>
-<?php
-// DB connection info
+  <?php
+  // DB connection info
 
-echo $_SERVER['SERVER_ADDR'];
-echo "hi";
-error_reporting(-1);
-ini_set('display_errors', 'On');
+  echo $_SERVER['SERVER_ADDR'];
+  echo "hi";
+  error_reporting(-1);
+  ini_set('display_errors', 'On');
 
-$host = "tcp:djdux8m8ie.database.windows.net,1433";
-$user = "DroneSense";
-$pwd = "Micro\$oft";
-$db = "DroneSense_db";
-try{
-    $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
-    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-}
-catch(Exception $e){
-    die(print_r($e));
-}
+  $host = "tcp:djdux8m8ie.database.windows.net,1433";
+  $user = "DroneSense";
+  $pwd = "Micro\$oft";
+  $db = "DroneSense_db";
+  try{
+      $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
+      $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+  }
+  catch(Exception $e){
+      die(print_r($e));
+  }
 
-$sql_select = "SELECT * FROM DroneSense.Data";
-$stmt = $conn->query($sql_select);
-while($row = mysql_fetch_array($stmt->fetchAll())) // loop to give you the data in an associative array so you can use it however.
-{
-     echo($row);
-}
-?>
-    Hey, Its working!!
+  $sql_select = "SELECT * FROM DroneSense.Data";
+  $stmt = $conn->query($sql_select);
+  while($row = mysql_fetch_array($stmt->fetchAll())) // loop to give you the data in an associative array so you can use it however.
+  {
+       echo($row);
+  }
+  ?>
 
+  <div class = "nav">
+    <div class = "container">
+      <div class="right">
+        <ul>
+          <li><a href="#">Preliminary Information</a></li>
+          <li><a href="#">Toxicology</a></li>
+          <li><a href="#">Environmental Information</a></li>
+          <li><a href="#">Drone Flyby Images</a></li>
+          <li><a href="#">Send Report</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <div class="jumbotron">
+    <div class="container">
+      <hr>
+      <h1>Biological / Ecological Drone Informatics</h1>
+      <hr>
+    </div>
+  </div>
+
+  <div class="Opening_Image">
+    <!-- add first image taken in here -->
+  </div>
+
+  <div class="dataPoints">
+    <h3>Preliminary Information | </h3>
+    <div class="Preliminary_Information">
+      <p><b>Name: </b> Johnny Appleseed</p>
+      <p><b>Sex: </b> Male</p>
+      <p><b>Age: </b> 20- 25</p>
+      <p><b>Height: </b> 5' 11"</p>
+      <p><b>Onsite Report: </b> At vero eos et accusamus et iusto odio
+        dignissimos ducimus qui blanditiis praesentium voluptatum deleniti
+         atque corrupti quos dolores et quas molestias excepturi sint
+         occaecati cupiditate non provident, similique sunt in culpa qui
+         officia deserunt mollitia animi, id est laborum et dolorum fuga.
+         Et harum quidem rerum facilis est et expedita distinctio. Nam
+         libero tempore, cum soluta nobis est eligendi optio cumque nihil
+         impedit quo minus id quod maxime placeat facere possimus, omnis
+         voluptas assumenda est, omnis dolor repellendus. Temporibus autem
+         quibusdam et aut officiis debitis aut rerum necessitatibus saepe
+         eveniet ut et voluptates repudiandae sint et molestiae non
+         recusandae. Itaque earum rerum hic tenetur a sapiente delectus,
+         ut aut reiciendis voluptatibus maiores alias consequatur aut
+         perferendis doloribus asperiores repellat.
+      </thead></p>
+    </div>
+
+    <h3>Toxicology | </h3>
+    <div class="Preliminary_Information">
+      <p><b>Breathalyzer: </b> <!-- add true false value here for a high enough point. --></p>
+      <p><b>Harmful Gasses Measurement: </b> <!-- add percentage here --></p>
+      <!-- add graph showing gas levels -->
+    </div>
+
+    <h3>Environmental Information | </h3>
+    <div class="Preliminary_Information">
+      <p><b>Average Temperature: </b> <!-- add temperature here --> F</p>
+      <p><b>Average Sunlight Exposure: </b> <!-- add light here --> %</p>
+      <p><b>Humidity: </b> <!-- add humidity percentage here --> %</p>
+      <!-- add light and humidity graphs here -->
+    </div>
+
+    <h3>Drone Flyby Images | </h3>
+    <!-- add a carasol for drone captured images here -->
+
+</div>
+<hr>
+<div class="dataPoints">
+  <h3>Send Report | </h3>
+    <div class="formStuff">
+      <form action="MAILTO:someone@example.com" method="post" enctype="text/plain">
+Name:<br>
+      <input type="text" name="name" value=""><br>
+    </br>
+E-mail:<br>
+      <input type="text" name="mail" value=""><br>
+    </br>
+Profession:<br>
+      <input type="text" name="subject" value=""><br>
+    </br>
+Comment:<br>
+      <input type="text" name="comment" value="" size="50"><br></br>
+      <input type="submit" value="Send">
+      <input type="reset" value="Reset">
+    </form>
+  </br>
+  </div>
+</div>
+</hr>
+
+<div class="footer">
+  <hr>
+  <p>Copyright Info Bla Bla Bla... Don't sue us if you hurt yourself.</p>
+</div>
 </body>
-
 </html>
---
+++ cpp/dronesence.ino
@@ -0,0 +1,99 @@
+// This #include statement was automatically added by the Particle IDE.
+#include "AzureMobileService/AzureMobileService.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "Adafruit_DHT/Adafruit_DHT.h"
+
+#define MYSERVICE "DroneSense"
+#define MYKEY "GoGEohBgAJkthyibFjeHLMYiZdfolH57"
+
+AzureMobileService ams;
+
+// Example testing sketch for various DHT humidity/temperature sensors
+// Written by ladyada, public domain
+
+#define DHTPIN 2     // what pin we're connected to
+
+
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT11		// DHT 11 
+//#define DHTTYPE DHT22		// DHT 22 (AM2302)
+//#define DHTTYPE DHT21		// DHT 21 (AM2301)
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+DHT dht(DHTPIN, DHTTYPE);
+
+float AnalogResolution = 4096;
+
+void setup(){
+	Serial.begin(9600); 
+	Serial.println("DHTxx test!");
+    ams.configure(MYSERVICE, MYKEY);
+	dht.begin();
+}
+
+void loop() {
+// Wait a few seconds between measurements.
+	delay(10000);
+
+// Reading temperature or humidity takes about 250 milliseconds!
+// Sensor readings may also be up to 2 seconds 'old' (its a 
+// very slow sensor)
+	float h = dht.getHumidity();
+// Read temperature as Celsius
+	float t = dht.getTempCelcius();
+// Read temperature as Farenheit
+	float f = dht.getTempFarenheit();
+  
+// Check if any reads failed and exit early (to try again).
+	if (isnan(h) || isnan(t) || isnan(f)) {
+		Serial.println("Failed to read from DHT sensor!");
+		return;
+	}
+
+// Compute heat index
+// Must send in temp in Fahrenheit!
+	float hi = dht.getHeatIndex();
+	float dp = dht.getDewPoint();
+	float k = dht.getTempKelvin();
+
+    String table="Data";
+    
+	Serial.print("Humid: "); 
+	Serial.print(h);
+	Serial.print("% - ");
+	Serial.print("Temp: "); 
+	Serial.print(t);
+	Serial.print("*C ");
+	Serial.print(f);
+	Serial.print("*F ");
+	Serial.print(k);
+	Serial.print("*K - ");
+	Serial.print("DewP: ");
+	Serial.print(dp);
+	Serial.print("*C - ");
+	Serial.print("HeatI: ");
+	Serial.print(hi);
+	Serial.println("*C");
+	Serial.println(Time.timeStr());
+	
+	Serial.print("Analog Sensor: ");
+	float MQ2 = analogRead(A0)/AnalogResolution;
+	Serial.println(MQ2);
+	
+
+	char buffer[100];
+    snprintf(buffer, sizeof(buffer), "{\"Humid\":%f, \"TempF\":%f,\"Gas_Sensor\":%f}",h,f,MQ2);
+    
+    
+    Serial.println("**************************************************************");
+    Serial.println("Publishing data from buffer to AMS");
+    Serial.println("**************************************************************");
+    ams.create(table, buffer);
+    
+}
+
